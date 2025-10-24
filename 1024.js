5e3fbf9bc22c95efb680f463811e507438a8a6ad
kangyumin <jsjm514@gmail.com>
2025-10-24 09:12:50 +0900

    1024

---
+++ 1024.js
@@ -0,0 +1,8 @@
+const numbers = [32,30,5,12,0,45]
+//for문 이용해서 numbers의 모든 숫자를 console에 찍기
+for (let i = 0; i < numbers.length; i++) {
+    //홀수만 출력
+    if (i % 2 === 1)
+    console.log(`${numbers[i]}는 짝수입니다.`)
+  console.log(numbers[i]);
+}
