afdd964d960ce0f176c9caaf0e1c00f7f819b5fb
kangyumin <jsjm514@gmail.com>
2025-10-25 17:29:43 +0900

    1024

--- 1024.js
+++ 1024.js
@@ -3,6 +3,5 @@ const numbers = [32,30,5,12,0,45]
 for (let i = 0; i < numbers.length; i++) {
     //홀수만 출력
     if (i % 2 === 1)
-    console.log(`${numbers[i]}는 짝수입니다.`)
-  console.log(numbers[i]);
+    console.log(`${numbers[i]}는 짝수입니다.`);
 }
