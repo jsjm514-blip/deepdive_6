b276c73b6de4e56bd2a79bd8b29193ada83a23a7
kangyumin <jsjm514@gmail.com>
2025-10-26 23:40:55 +0900

    1024

--- 1024.js
+++ 1024.js
@@ -1,7 +1,17 @@
 const numbers = [32,30,5,12,0,45]
-//for문 이용해서 numbers의 모든 숫자를 console에 찍기
+const str = 'Hello, World'
+//01 홀수만 출력하기
 for (let i = 0; i < numbers.length; i++) {
-    //홀수만 출력
-    if (i % 2 === 1)
-    console.log(`${numbers[i]}는 짝수입니다.`);
+    if (numbers[i] % 2 === 1)
+    console.log(`${numbers[i]}는 홀수입니다.`);
 }
+
+//02 길이 출력하기
+console.log(str.length)
+
+//03 총합 구하기
+let result = 0;
+for (let i = 0; i < 6; i++) {
+    result = result + numbers[i]
+}
+console.log(`총합은 ${result}입니다.`)
