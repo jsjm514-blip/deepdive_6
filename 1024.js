e015691e8488de1ef6eceb256f859a67bcde6ee6
kangyumin <jsjm514@gmail.com>
2025-10-27 00:15:31 +0900

    1024 과제

--- 1024.js
+++ 1024.js
@@ -1,17 +1,24 @@
 const numbers = [32,30,5,12,0,45]
 const str = 'Hello, World'
-//01 홀수만 출력하기
+//01. 홀수만 출력하기
 for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] % 2 === 1)
     console.log(`${numbers[i]}는 홀수입니다.`);
 }
 
-//02 길이 출력하기
+//02. 길이 출력하기
 console.log(str.length)
 
-//03 총합 구하기
+//03. 총합 구하기
 let result = 0;
 for (let i = 0; i < 6; i++) {
     result = result + numbers[i]
 }
 console.log(`총합은 ${result}입니다.`)
+
+//04. 20 이상의 수의 개수를 출력하시오.
+let count = 0;
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 20) {count++}
+}
+console.log(`20이상의 숫자는 ${count}개 입니다.`)
