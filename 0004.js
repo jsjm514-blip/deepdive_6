255355f2687c50b4ea4718a425243991a931f526
kangyumin <jsjm514@gmail.com>
2025-11-12 23:33:46 +0900

    review

--- 0004.js
+++ 0004.js
@@ -37,5 +37,54 @@ console.log(sum(3,7))
 
 //07 -> setTimeout에 대해 공부하기
 setTimeout(function() {
-  console.log("Hello");
+    console.log("Hello");
 }, 1000);
+
+//08 -> callback에 대해 공부하기
+function printTwice(callback) {
+    callback();
+    callback();
+}
+printTwice(function() {console.log("Hi");});
+
+//09
+function makeArray(a,b) {
+    return[a,b]
+}
+console.log(makeArray(1, 2));
+
+//10
+function makePerson(name,age) {
+    return {name: name,age: age}
+}
+console.log(makePerson("Jane", 20));
+
+console.clear();
+
+//Function 연습퀴즈 2
+//01
+function isEven(a){
+    return a%2===0;
+}
+console.log(isEven(4));
+
+//02 -> 이거 생각치도 못함
+function sumToN (n){
+    let sum =0;
+    for (let i=0;i<=n;i++) {
+        sum += i
+    }
+    return sum
+}
+console.log(sumToN(5));
+
+//03
+function sumArray(a) {
+    return a.reduce((acc,cur)=>acc+cur,0)
+}
+console.log(sumArray([1,2,3]));
+
+//04
+function max(a) {
+    
+}
---
+++ 0005.js
@@ -0,0 +1,15 @@
+//변수 퀴즈 모음
+//11
+const name ="유민"
+console.log(`안녕하세요, ${name}님!`)
+
+//12
+num1 = 3;
+num2 = 5;
+console.log(num1+num2)
+
+//13
+a = 6;
+b = 2;
+console.log(a*b)
+console.log(a/b)
